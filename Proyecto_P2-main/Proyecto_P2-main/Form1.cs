3c42e353d4dce1c5dc58c276458c5841ca36ed92
JoseCarlosPerezValencia <163448548+JoseCarlosPerezValencia@users.noreply.github.com>
2024-05-29 20:40:54 -0600

    Update Form1.cs

--- Proyecto_P2-main/Proyecto_P2-main/Form1.cs
+++ Proyecto_P2-main/Proyecto_P2-main/Form1.cs
@@ -16,21 +16,36 @@ namespace persona_herencia
 {
     public partial class Form1 : Form
     {
-        public Form1()
+         public Form1()
         {
             InitializeComponent();
         }
 
-        private void guardar_Click(object sender, EventArgs e) //esta parte del codigo fue un reeciclaje de la actividad del cajero automatico
+        private void guardar_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Los datos han sido guardados correctamente");// esto se hace de tarea todos los valores
-            // deben de almacenarse en su
-            // caja de memoria correspondiente
-            // usando la clase hija que corresponda
+            if (radioalumno.Checked)
+            {
+                MessageBox.Show("DATOS GUARDOS" + "\nNombre: " + textBox1.Text + "\nFecha de Nac: " + textBox2.Text
+                + "\nEdad: " + textBox3.Text + "\nMatricula: " + textBox4.Text + "\nCarrera: " + textBox5.Text);
+            }
+            else
+            {
+                if (radiodocente.Checked || radioempleado.Checked)
+                {
+                    MessageBox.Show("DATOS GUARDOS" + "\nNombre: " + textBox1.Text + "\nFecha de Nac: " + textBox2.Text
+              + "\nEdad: " + textBox3.Text + "\nDNI: " + textBox4.Text + "\nPuesto: " + textBox5.Text + "\nSueldo: " + textBox6.Text);
+                }
+                else
+                {
+                    MessageBox.Show("DATOS GUARDOS" + "\nNombre: " + textBox1.Text + "\nFecha de Nac: " + textBox2.Text
+               + "\nEdad: " + textBox3.Text);
+                }
+            }
         }
 
-        private void limpiar_Click(object sender, EventArgs e) //de igual manera que se hizo con el codigo del boton "guardar" (reciclando el codigo que se ocupo para el cajero automatico)
+        private void limpiar_Click(object sender, EventArgs e)
         {
+            //Limpieza de las cajas de texto
             textBox1.Clear();
             textBox2.Clear();
             textBox3.Clear();
@@ -40,30 +55,118 @@ namespace persona_herencia
             // debe de borrar las cajas de texto
         }
 
-        private void Salir_Click(object sender, EventArgs e) //de igual manera que se aplico en limpiar y guardar, se hizo este codigo
+        private void Salir_Click(object sender, EventArgs e)
         {
+            //Cerrar programa
             Application.Exit();// se hace de tarea
             // debe de terminar la ejecucion de la interfaz
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void validar_Click(object sender, EventArgs e)
         {
+            if (radioalumno.Checked)
+            {
 
+                string nom = textBox1.Text;
+                string fech = textBox2.Text;
+                int edad = Convert.ToInt32(textBox3.Text);
+                int mat = Convert.ToInt32(textBox4.Text);
+                string carrera = textBox5.Text;
+                Alumno alumno = new Alumno(nom, edad, fech, carrera, mat);
+            }
+            else
+            {
+                if (radioempleado.Checked)
+                {
+
+                    string nom = textBox1.Text;
+                    string fech = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                    int mat = Convert.ToInt32(textBox4.Text);
+                    string puesto = textBox5.Text;
+                    float sueldo = Convert.ToSingle(textBox6.Text);
+                    Empleado empleado = new Empleado(nom, edad, fech, puesto, sueldo);
+                }
+                else if (radiodocente.Checked)
+                {
+                    string nom = textBox1.Text;
+                    string fech = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                    int mat = Convert.ToInt32(textBox4.Text);
+                    string puesto = textBox5.Text;
+                    float sueldo = Convert.ToSingle(textBox6.Text);
+                    Docente docente = new Docente(nom, edad, fech, puesto, sueldo);
+                }
+                else
+                {
+                    string nom = textBox1.Text;
+                    string fech = textBox2.Text;
+                    int edad = Convert.ToInt32(textBox3.Text);
+                }
+            }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void radioalumno_CheckedChanged(object sender, EventArgs e)
         {
-
+            string rutaImagen = "D:\\cuarto semestre\\poo\\imagenes\\alumno.jpg";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label6.Visible = false;
+            textBox6.Visible = false;
+            if (label4.Visible == false && textBox4.Visible == false && label5.Visible == false && textBox5.Visible == false) //comprobar que los label y textbox estem
+                                                                                                                             //apagados o prendidos
+            {
+                label4.Visible = true;
+                textBox4.Visible = true;
+                label5.Visible = true;
+                textBox5.Visible = true;
+            }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
-
+            string rutaImagen = "D:\\cuarto semestre\\poo\\imagenes\\persona.jpg";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label4.Visible = false;
+            textBox4.Visible = false;
+            label5.Visible = false;
+            textBox5.Visible = false;
+            label6.Visible = false;
+            textBox6.Visible = false;
         }
+
+        private void radioempleado_CheckedChanged(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\cuarto semestre\\poo\\imagenes\\empleado.jpg";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label4.Visible = true;
+            textBox4.Visible = true;
+            label5.Visible = true;
+            textBox5.Visible = true;
+            label6.Visible = true;
+            textBox6.Visible = true;
+        }
+
+        private void radiodocente_CheckedChanged(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\cuarto semestre\\poo\\imagenes\\maestro.jpg";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            label4.Visible = true;
+            textBox4.Visible = true;
+            label5.Visible = true;
+            textBox5.Visible = true;
+            label6.Visible = true;
+            textBox6.Visible = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string rutaImagen = "D:\\cuarto semestre\poo\\imagenes\\bienvenida.jpg";
+            pictureBox1.BackgroundImage = Image.FromFile(rutaImagen);
+            radioalumno.Checked = false;
+            radioButton1.Checked = false;
+            radiodocente.Checked = false;
+            radioempleado.Checked = false;
+        }
+message.txt
+7 KB
     }
 }
